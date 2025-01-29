b068d28aa3726d0eb5d4a86d17e049e5f5479883
WhiteBunnyI <144366124+WhiteBunnyI@users.noreply.github.com>
2025-01-29 07:39:57 +0200

    .

---
+++ ASD/4 semester/Task3/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace asd
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            if (!File.Exists("../../../Text.txt"))
+            {
+                var file = File.Create("../../../Text.txt");
+                file.Close();
+            }
+            string text = File.ReadAllText("../../../Text.txt");
+            Console.WriteLine("Введите образец для поиска: ");
+            string sample = Console.ReadLine();
+            if(sample == null || sample == "")
+            {
+                Console.WriteLine("Образец не найден");
+                return;
+            }
+            int state = 0;
+            for(int i = 0; i < text.Length; i++)
+            {
+                
+                if (state < sample.Length && text[i] == sample[state])
+                    ++state;
+                else
+                {
+                    state = 0;
+                    for(int o = 0; o < sample.Length; o++)
+                    {
+                        if (sample[sample.Length - 1 - o] == text[i - state])
+                        {
+                            state++;
+                        }
+                        else
+                        {
+                            state = 0;
+                        }
+
+                    }
+                }
+                if (state == sample.Length)
+                {
+                    Console.WriteLine("Подслово было найдено! Его позиция: " + (i - state + 1));
+                }
+            }
+        }
+    }
+
+    
+}
---
+++ ASD/4 semester/Task4/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace Task4
+{
+    internal class Program
+    {   //abab
+        static int[] PreficsFunc(string word)
+        {
+            int[] result = new int[word.Length];
+            int j = 0;
+            for(int i = 1; i < word.Length; i++)
+            {
+                while (j > 0 && word[i] != word[j])
+                    j = result[j - 1];
+
+                if (word[i] == word[j])
+                    j++;
+
+                result[i] = j;
+            }
+            return result;
+        }
+        static void Main()
+        {
+            if (!File.Exists("../../../Text.txt"))
+            {
+                var file = File.Create("../../../Text.txt");
+                file.Close();
+            }
+
+            string text = File.ReadAllText("../../../Text.txt");
+            Console.WriteLine("Введите образец для поиска: ");
+            string sample = Console.ReadLine();
+            if(sample == null || sample.Length == 0)
+            {
+                Console.WriteLine("Образец не найден");
+                return;
+            }
+            int[] prefics = PreficsFunc(sample);
+            int j = 0;
+            for(int i = 0; i < text.Length; i++)
+            {
+                while(j > 0 && text[i] != sample[j])
+                {
+                    j = prefics[j - 1];
+                }
+                if (text[i] == sample[j])
+                {
+                    j++;
+                }
+                if(j == sample.Length)
+                {
+                    Console.WriteLine("Подслово было найдено! Его позиция: " + (i - j + 1));
+                    j = prefics[j - 1];
+                }
+            }
+        }
+    }
+}
